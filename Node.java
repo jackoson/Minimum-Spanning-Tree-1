b4612850e65cc7fcde3083a52ecb06f8407d228c
jackoson <jackosonmail-github@yahoo.co.uk>
2015-02-19 13:13:01 +0000

    Original Files

---
+++ Node.java
@@ -0,0 +1,30 @@
+/**
+ * Class representing a node of the graph
+ */
+public class Node
+{
+    private String name;
+
+    /**
+     * Node constructor
+     * @param n Name that will be given to the node
+     */
+    Node(String n)
+    {
+        name = n;
+    }
+
+    /**
+     * Function that gets the name that has been assigned to the node
+     * @return
+     */
+    String name()
+    {
+        return name;
+    }
+    
+    public String toString() 
+    {
+    	return name;
+    }
+}
